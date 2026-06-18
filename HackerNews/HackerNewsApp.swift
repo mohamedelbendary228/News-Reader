00777969dd4a5273412bc6c66eff00f2ea939fad
mohamedelbendary <mohamedelbendary228@gmail.com>
2026-06-18 15:37:10 +0300

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Mohamed Elbendary on 18/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
